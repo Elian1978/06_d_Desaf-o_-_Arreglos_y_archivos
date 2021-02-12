145e2fd29ffa9d9d5a1cd9215c1de9ae36a447a6
Elian Rojas <impresosgosen@gmail.com>
2021-02-11 23:12:45 -0600

    Proyections ready

--- proyecciones.rb
+++ proyecciones.rb
@@ -1,7 +1,9 @@
+#nuevo archivo agregamos decimales, para leer y hacer espacio
 original_data = open('ventas_base.txt').read.split(',') 
 data = original_data.map { |data| data.to_f}
-#nuevo archivo agregamos decimales, para leer y hacer espacio
 
+
+#crear un método que reciba cuatro argumentos
 def proyections(ventas_base, augment, start_array, end_array)    
     a = ventas_base.map.with_index do |sales, index|
         if index >= start_array && index <= end_array
@@ -12,23 +14,24 @@ def proyections(ventas_base, augment, start_array, end_array)
     end
     return a
 end
-#crear un método que reciba cuatro argumentos
+
 
 def sum_proyection(array)
     array.inject(0) { |acc, sale| acc + sale}
 end
 
+#la primera mitad del semestre(1 Trimestre)se vende un 10% adicional
 percent_1 = 1.10
 start_array1 = 0
 end_array1 = 5
 result_1 = "%0.2f" % [sum_proyection(proyections(data, percent_1, start_array1, end_array1))]
-#la primera mitad del semestre se vende un 10% más
 
+#la segunda mitad del semestre se vende un 20% más(2 TRIMESTRE)
 percent_2 = 1.20
 start_array2 = 6
 end_array2 = 11
 result_2 = "%0.2f" % [sum_proyection(proyections(data, percent_2, start_array2, end_array2))]
-#la segunda mitad del semestre se vende un 20% más
 
+#IMPRIMO RESULTADOS EN NUEVOS ARCHIVOS
 File.write('resultados.data', result_1.to_s + ("\n"))
 File.write('resultados.data', result_2, mode: 'a')
